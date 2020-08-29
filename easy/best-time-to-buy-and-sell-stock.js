a600d7f35ae014ba4619d200c39423f55672e5d7
David Hodgson <hodgsondh@gmail.com>
2020-08-28 21:10:44 -0400

    Added #121

---
+++ easy/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        for (let j = i; j < prices.length; j++){
+            if ( prices[j] > prices[i]) {
+                let profit = prices[j] - prices[i];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+
+            }
+        }
+    }
+
+    return maxProfit;
+
+};
+
+let prices = [7,6,4,3,1];
+console.log(maxProfit(prices))
